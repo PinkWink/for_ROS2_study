67af0d74f91895434570452b615fe1da124fde0b
PinkWink <pinkwink.korea@gmail.com>
2024-03-10 08:55:04 +0900

    Update alias_settings.sh

--- bash_settings/alias_settings.sh
+++ bash_settings/alias_settings.sh
@@ -1,3 +1,7 @@
+# argcomplete for ros2 & colcon
+alias tmp1="eval \"\$(register-python-argcomplete3 ros2)\""
+alias tmp2="tmp1; eval \"\$(register-python-argcomplete3 colcon)\""
+
 # ALIAS settings
 ID=13
 
@@ -6,18 +10,16 @@ alias killgazebo="killall gzserver gzclient"
 alias sz="source ~/.zshrc; echo \"Zshrc is reloaded!\""
 alias sb="source ~/.bashrc; echo \"Bashrc is reloaded!\""
 
-
-sh is reloaded!\""
 alias ros_domain="export ROS_DOMAIN_ID=\$ID; echo \"ROS_DOMAINID is set to \$ID !\""
-alias humble="source /opt/ros/humble/setup.zsh; ros_domain; echo \"ROS2 Humble is activated\""
+alias humble="source /opt/ros/humble/setup.bash; ros_domain; echo \"ROS2 Humble is activated\""
 
 ws_setting()
 {
 	humble
-	source ~/$1/install/local_setup.zsh
+	source ~/$1/install/local_setup.bash
 	echo "$1 workspace is activated."
 	tmp2
 }
 
-alias  my_mobile="ws_setting \"my_mobile\""
-alias  urdf_study="ws_setting \"urdf_study\""
+alias  ros2_study="ws_setting \"ros2_study\""
+
